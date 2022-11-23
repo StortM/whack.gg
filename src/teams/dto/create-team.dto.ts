9b523352a7621d93c0253051963cd254735bc83a
Julius Stenbæk Christoffersen <julius@adnudging.com>
2022-11-23 10:48:15 +0100

    Added validation to all DTOs except teams_bans_champions

--- src/teams/dto/create-team.dto.ts
+++ src/teams/dto/create-team.dto.ts
@@ -1,4 +1,12 @@
+import { IsBoolean, IsNotEmpty, IsNumber, Min } from 'class-validator'
+
 export class CreateTeamDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
   matchId!: number
+
+  @IsBoolean()
+  @IsNotEmpty()
   matchWon!: boolean
 }
