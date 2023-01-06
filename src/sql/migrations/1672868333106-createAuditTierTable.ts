c0eb9e2ecf168b1f1c1d3cf81d5afdb2f671cc36
Julius Stenbæk Christoffersen <julius@adnudging.com>
2023-01-06 16:21:48 +0100

    Fixed merge conflict in package.json

--- src/sql/migrations/1672868333106-createAuditTierTable.ts
+++ src/sql/migrations/1672868333106-createAuditTierTable.ts
@@ -8,8 +8,9 @@ export class auditingTrigger1672868333106 implements MigrationInterface {
       STAMP TIMESTAMP NOT NULL,
       USER_ID CHAR(20) NOT NULL,
       VALUE VARCHAR(25)
-  );
-    `)
+    );
+    CREATE INDEX IF NOT EXISTS AUDIT_TIER_STAMP ON AUDIT_TIER(STAMP);
+  `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
